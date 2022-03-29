564ebb19d204703d4965067b1a5981655dbc7530
Kim Daniel <64163182+KIMdaniiell@users.noreply.github.com>
2022-03-29 07:27:12 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,26 @@
-import computation.SquareMatrixWrapper;
+import model.SquareMatrixWrapper;
 import io.MatrixInputManager;
-
-import java.util.Arrays;
+import model.computation.GaussSeidelConditionChecker;
+import model.computation.GaussSeidelMethodSolver;
 
 public class Main {
 
     public static void main(String[] args) {
         MatrixInputManager matrixInputManager = new MatrixInputManager();
-        SquareMatrixWrapper squareMatrixWrapper = matrixInputManager.readSquareMatrixWrapper();
+        SquareMatrixWrapper squareMatrixWrapper = matrixInputManager.readSquareMatrixWrapper("input.txt");
 
-        squareMatrixWrapper.printMatrixA();
+        /**squareMatrixWrapper.printMatrixA();
+        System.out.println();
         squareMatrixWrapper.printMatrixB();
+        System.out.println();**/
+
+        GaussSeidelConditionChecker gaussSeidelConditionChecker = new GaussSeidelConditionChecker();
+        if (gaussSeidelConditionChecker.check(squareMatrixWrapper)) {
+            System.out.println("###Проверка пройдена");
+            GaussSeidelMethodSolver gaussSeidelMethodSolver = new GaussSeidelMethodSolver();
+            gaussSeidelMethodSolver.solve(squareMatrixWrapper);
+        } else {
+            //TODO
+        }
     }
 }
